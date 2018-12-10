4c622e55028416c6d70fec508fd43ce70c0d4eaa
joshua <whatthejoshuaperez@gmail.com>
2018-12-11 01:22:35 +0800

    ...
    Signed-off-by: joshua <whatthejoshuaperez@gmail.com>

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,8 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (!Auth::guard($guard)->check()) {
-            return redirect(route('guest.user-login',['storeName' => $request->storeName]));
+        if (Auth::guard($guard)->check()) {
+            return redirect(route('auth.shops',['storeName' => $request->storeName]));
         }
 
         return $next($request);
