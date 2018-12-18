9576938c6d0b7e54fedbebe469123356e4d46ec5
jshprz <whatthejoshuaperez@gmail.com>
2018-12-18 19:31:45 +0800

    ...
    Signed-off-by: jshprz <whatthejoshuaperez@gmail.com>

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('guest.user-login');
+            return route('auth.shops');
         }
     }
 }
