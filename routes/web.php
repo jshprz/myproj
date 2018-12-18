45cff9daa29db3ac40134cea17ba5c3b3c7fcec9
jshprz <whatthejoshuaperez@gmail.com>
2018-12-18 18:55:06 +0800

    ...
    Signed-off-by: jshprz <whatthejoshuaperez@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::group(['middleware' => 'guest', 'as' => 'guest.'], function(){
     Route::get('/user-registration','Auth\RegisterController@index')->name('user-register');
 });
 
+
+
 Route::group(['middleware' => 'auth','as' => 'auth.'], function(){
     Route::get('/buyer-logout',function(){
 
@@ -30,7 +32,7 @@ Route::group(['middleware' => 'auth','as' => 'auth.'], function(){
         return redirect()->route('guest.user-login');
 
 })->name('buyerLogout');
-    
+
     Route::get('/','ShopController@index')->name('shops');
     Route::get('/cart','ShopController@viewCart')->name('viewcart');
     Route::post('/checkout','PaymentController@index')->name('checkout');
