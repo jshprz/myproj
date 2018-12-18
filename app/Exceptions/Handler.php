96912b83c9ac22821e44c7430f5bb3b501dab14f
jshprz <whatthejoshuaperez@gmail.com>
2018-12-18 19:46:26 +0800

    ...
    Signed-off-by: jshprz <whatthejoshuaperez@gmail.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return redirect('/');
+        return parent::render($request, $exception);
     }
 }
